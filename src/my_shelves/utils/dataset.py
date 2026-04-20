df58690718d164ecabbf92ce32ed32ae29e77590
joelle six <j.sixke@gmail.com>
2026-04-20 14:53:12 +0200

    added problems pylint

--- src/my_shelves/utils/dataset.py
+++ src/my_shelves/utils/dataset.py
@@ -1,15 +1,20 @@
 import pandas as pd
 
 def get_books(lang='ENG', nrows=None):
+    '''
+    get books
+    '''
     file_name = f'../../data/books_{lang}_mini.csv'
-    if(nrows==None):
+    if nrows is None:
         return pd.read_csv(file_name)
-    else:
-        return pd.read_csv(file_name, nrows=nrows)
+    return pd.read_csv(file_name, nrows=nrows)
 
 def get_reviews(lang='ENG', nrows=None):
+    '''
+    get reviews
+    '''
+
     file_name = f'../../data/reviews_{lang}_mini.csv'
-    if(nrows==None):
+    if nrows is None:
         return pd.read_csv(file_name)
-    else:
-        return pd.read_csv(file_name, nrows=nrows)
+    return pd.read_csv(file_name, nrows=nrows)
