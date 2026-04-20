27fe6c1070d12974e4379425242244b3f8141301
christophezito <christophezito@gmail.com>
2026-04-20 16:52:38 +0200

    Add data_dir to utils.dataset functions.

--- src/my_shelves/utils/dataset.py
+++ src/my_shelves/utils/dataset.py
@@ -1,15 +1,17 @@
 """
 Module to retrieve data from csv
 """
-
 import pandas as pd
 
-def get_books(lang: str = 'ENG', nrows: int = None) -> pd.DataFrame:
+
+def get_books(data_dir: str, lang: str = 'ENG', nrows: int = None) -> pd.DataFrame:
     """
     Load a books dataset from a CSV file based on the specified language.
 
     Parameters
     ----------
+    data_dir : str
+        The data path.
     lang : str, default='ENG'
         Language code used to select the dataset file
         (e.g., 'ENG', 'FRA', 'DUT').
@@ -28,18 +30,20 @@ def get_books(lang: str = 'ENG', nrows: int = None) -> pd.DataFrame:
     '../../data/books_<lang>_mini.csv'
     """
 
-    file_name = f'../../data/books_{lang}_mini.csv'
+    file_name = f'{data_dir}/books_{lang}_mini.csv'
     if nrows is None:
         return pd.read_csv(file_name)
     return pd.read_csv(file_name, nrows=nrows)
 
 
-def get_reviews(lang: str = 'ENG', nrows: int = None) -> pd.DataFrame:
+def get_reviews(data_dir: str, lang: str = 'ENG', nrows: int = None) -> pd.DataFrame:
     """
     Load a reviews dataset from a CSV file based on the specified language.
 
     Parameters
     ----------
+    data_dir : str
+        The data path.
     lang : str, default='ENG'
         Language code used to select the dataset file
         (e.g., 'ENG', 'FRA', 'DUT').
@@ -58,7 +62,7 @@ def get_reviews(lang: str = 'ENG', nrows: int = None) -> pd.DataFrame:
     '../../data/reviews_<lang>_mini.csv'
     """
 
-    file_name = f'../../data/reviews_{lang}_mini.csv'
+    file_name = f'{data_dir}/reviews_{lang}_mini.csv'
     if nrows is None:
         return pd.read_csv(file_name)
     return pd.read_csv(file_name, nrows=nrows)
