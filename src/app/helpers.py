7ea16c51ff8dd06a78d211f113bb6893a93e8e95
joelle six <j.sixke@gmail.com>
2026-04-23 18:17:15 +0200

    create params.py and call this variable for the URL_APIs

--- src/app/helpers.py
+++ src/app/helpers.py
@@ -38,13 +38,8 @@ Notes
 """
 import requests
 import streamlit as st
+from params import  API_URL_BOOK
 
-# HOST = '127.0.0.1:8000'
-HOST = 'my-shelves-image-151819310613.europe-west1.run.app'
-API_URL_COUNTRY = f'http://{HOST}/country'
-API_URL_BOOK_IDS_BY_COUNTRY = f"http://{HOST}/books/by-country"
-API_URL_BOOKS = f"http://{HOST}/books"
-API_URL_BOOK = f"http://{HOST}/read"
 BOOK_DETAILS_CSS = """
 <style>
 .book-card {
---
+++ src/app/params.py
@@ -0,0 +1,7 @@
+# HOST = '127.0.0.1:8000'
+HOST = 'my-shelves-image-151819310613.europe-west1.run.app'
+
+API_URL_COUNTRY = f"http://{HOST}/country"
+API_URL_BOOK_IDS_BY_COUNTRY = f"http://{HOST}/books/by-country"
+API_URL_BOOKS = f"http://{HOST}/books"
+API_URL_BOOK = f"http://{HOST}/read"
