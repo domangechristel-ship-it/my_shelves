29375e845c4bf655b24948e7f0421fb416121cc5
joelle six <j.sixke@gmail.com>
2026-04-27 11:17:42 +0200

    refactor location to use prefect

--- src/my_shelves/ml/location/train.py
+++ src/my_shelves/ml/location/train.py
@@ -1,14 +1,24 @@
 
+import os
+import pandas as pd
 from prefect import task, flow
 from prefect.artifacts import create_table_artifact
-
+from my_shelves.ml.classification.params import OUTPUT_FOLDER
+from my_shelves.ml.location.location import Location
 
 PREFECT_FLOW_NAME="locations"
 
 
 @task
-def get_locations():
-    return
+def get_locations(X: pd.DataFrame, n_rows: str = "10k") -> str:
+    output_csv = f"{OUTPUT_FOLDER}/locations_{n_rows}.csv"
+
+    feature_loc = Location()
+    df_locations = feature_loc.get_locations(X)
+
+    df_locations.to_csv(output_csv, index=False)
+
+    return output_csv
 
 
 @flow(name=PREFECT_FLOW_NAME)
